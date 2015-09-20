5c6a5c114946350a9403f960d374de2f8496803c
REAANDREW <email@andrewrea.co.uk>
2015-09-20 18:58:49 +0000

    Building the first test to get a list of urls being requested

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
 
-func main(){
-	fmt.Println("Done")
+func check(err error) {
+	if err != nil {
+		fmt.Errorf("%v",err)
+		panic(err.Error())
+	}
+}
+
+func main() {
+	filePath := kingpin.Flag("file", "Urls file").Short('f').String()
+	kingpin.Parse()
+
+	absolutePath, err := filepath.Abs(*filePath)
+	check(err)
+	file, err := os.Open(absolutePath)
+	check(err)
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		client := &http.Client{}
+		line := scanner.Text()
+		req, _ := http.NewRequest("GET", line, nil)
+		_, _ = client.Do(req)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,63 @@
 package main_test
 
 import (
-	. "github.com/reaandrew/code-named-something"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"net/http/httptest"
+	"net"
+	"net/http"
+	"strings"
+	"os/exec"
+	"io/ioutil"
+	"os"
+	"log"
+	"fmt"
+	"strconv"
 )
 
+func containsString(list []string, expected string) bool {
+	for _, current := range list {
+		if current == expected {
+			return true
+		}
+	}
+	return false
+}
+
 var _ = Describe("Main", func() {
-	It("Something", func(){
-		Expect(1).To(Equal(1))
+	It("Makes a http get request to each url in a file", func() {
+		port := 8000
+		list := `
+		http://127.0.0.1:$port/A
+		http://127.0.0.1:$port/B
+		http://127.0.0.1:$port/C
+		`
+
+		urlsVisited := []string{}
+
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			panic("BANG")
+			urlsVisited = append(urlsVisited, r.URL.Path)
+		})
+
+		server := httptest.NewUnstartedServer(handler)
+		server.Listener, _ = net.Listen("tcp", ":"+strconv.Itoa(port))
+		server.Start()
+		defer server.Close()
+		file, _ := ioutil.TempFile(os.TempDir(), "prefix")
+		file.WriteString(strings.Replace(list,"$port", strconv.Itoa(port), -1))
+		defer os.Remove(file.Name())
+
+		log.Printf(fmt.Sprintf("Temp filename = %s", file.Name()))
+		cmd := exec.Command("./code-named-something", "-f", file.Name())
+		output, err := cmd.CombinedOutput()
+		fmt.Println(err)
+		fmt.Println(string(output))
+
+		Expect(err).To(BeNil())
+		Expect(containsString(urlsVisited,"/A")).To(Equal(true))
+		Expect(containsString(urlsVisited,"/B")).To(Equal(true))
+		Expect(containsString(urlsVisited,"/C")).To(Equal(true))
+		})
 	})
-})

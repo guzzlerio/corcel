2a6bdef6e4517ca1fa5db4ebb499211841cd6288
REAANDREW <email@andrewrea.co.uk>
2015-09-20 21:08:30 +0000

    Moving the github deployment to a script

---
+++ ci/deploy-to-github.sh
@@ -0,0 +1,9 @@
+if [ "$TRAVIS_BRANCH" == "release" ]
+then
+	ghr --username reaandrew --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
+fi
+
+if [ "$TRAVIS_BRANCH" == "develop" ]
+then
+	ghr --username reaandrew --token $GITHUB_TOKEN --replace --prerelease latest dist/  
+fi

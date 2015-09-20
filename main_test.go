30c2d79f1f3c3c1a0c74a4f4389fe2fbcbf37881
REAANDREW <email@andrewrea.co.uk>
2015-09-20 14:38:32 +0000

    Added travis file and also added the main package to the test file

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main_test
 
 import (
-	. "UNKNOWN_PACKAGE_PATH"
+	. "github.com/reaandrew/code-named-something"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Main", func() {
-
+	It("Something", func(){
+		Expect(1).To(Equal(1))
+	})
 })

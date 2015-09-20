83a9e6f1924ba15c542cabd9556c976405e8d17c
REAANDREW <email@andrewrea.co.uk>
2015-09-20 21:40:57 +0000

    Updating build deplyments

--- ci/deploy-to-github.sh
+++ ci/deploy-to-github.sh
@@ -1,6 +1,11 @@
-if [ "$TRAVIS_BRANCH" == "release" ]
+if [ "$TRAVIS_BRANCH" == "master" ]
 then
 	ghr --username reaandrew --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
+fi
+
+if [ "$TRAVIS_BRANCH" == "release" ]
+then
+	ghr --username reaandrew --token $GITHUB_TOKEN --replace --prerelease pre-release dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]

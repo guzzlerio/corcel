80bc635204dd163dd668fb86ae17d84dc7593336
REAANDREW <email@andrewrea.co.uk>
2015-09-24 18:35:20 +0000

    Created a CommandLineLexer to handle command line arguments in the form of a string and not already aplit into an array.  This now supports adding Headers to the HTTP Request using the -H flag

--- RequestAdapter.go
+++ RequestAdapter.go
@@ -6,22 +6,28 @@ import (
 )
 
 type RequestAdapter struct {
+	commandLineLexer *CommandLineLexer
 }
 
 func NewRequestAdapter() RequestAdapter {
-	return RequestAdapter{}
+	return RequestAdapter{
+		commandLineLexer : NewCommandLineLexer(),
+	}
 }
 
 func (instance RequestAdapter) Create(line string) (*http.Request, error) {
-	line = strings.TrimSpace(line)
-	lineSplit := strings.Split(line, " ")
-	if len(lineSplit) > 1 {
-		req, err := http.NewRequest("GET", lineSplit[0], nil)
-		if lineSplit[1] == "-X" {
-			req.Method = lineSplit[2]
+	lineSplit := instance.commandLineLexer.Lex(line)
+	req, err := http.NewRequest("GET", lineSplit[0], nil)
+	for index, _ := range lineSplit {
+		if lineSplit[index] == "-X" {
+			req.Method = lineSplit[index+1]
 		}
-		return req, err
-	} else {
-		return http.NewRequest("GET", line, nil)
+		if lineSplit[index] == "-H" {
+			value := strings.Trim(lineSplit[index+1],"\"")
+
+			valueSplit := strings.Split(value,":")
+			req.Header.Set(strings.TrimSpace(valueSplit[0]),strings.TrimSpace(valueSplit[1]))
+		}
 	}
+	return req, err
 }

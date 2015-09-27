3ce2e3a6ea4b851140351682750566861c5e5ee8
REAANDREW <email@andrewrea.co.uk>
2015-09-27 21:18:14 +0000

    Moved the gox into the deploy shell script and updated the travis file to not invoke gox

--- ci/deploy-to-github.sh
+++ ci/deploy-to-github.sh
@@ -1,14 +1,17 @@
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
+    gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
 	ghr --username reaandrew --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "release" ]
 then
+    gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
 	ghr --username reaandrew --token $GITHUB_TOKEN --replace --prerelease pre-release dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]
 then
+    gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
 	ghr --username reaandrew --token $GITHUB_TOKEN --replace --prerelease latest dist/  
 fi

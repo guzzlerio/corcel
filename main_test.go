245621201a37cde1aa1103816efec5c831dec2c5
REAANDREW <email@andrewrea.co.uk>
2015-09-27 09:51:23 +0000

    Renamed server to TestServer

--- main_test.go
+++ main_test.go
@@ -13,22 +13,22 @@ import (
 var (
 	SUPPORTED_HTTP_METHODS         = []string{"GET", "POST", "PUT", "DELETE"}
 	HTTP_METHODS_WITH_REQUEST_BODY = []string{"POST", "PUT", "DELETE"}
-	server                         *RequestRecordingServer
+	TestServer                     *RequestRecordingServer
 	TEST_PORT                      = 8000
 )
 
-func sutUrl(path string) string{
-	return fmt.Sprintf("http://localhost:%d%s",TEST_PORT,path)
+func sutUrl(path string) string {
+	return fmt.Sprintf("http://localhost:%d%s", TEST_PORT, path)
 }
 
 var _ = BeforeSuite(func() {
 	configureLogging()
-	server = CreateRequestRecordingServer(TEST_PORT)
-	server.Start()
+	TestServer = CreateRequestRecordingServer(TEST_PORT)
+	TestServer.Start()
 })
 
-var _ = AfterSuite(func(){
-	server.Stop()
+var _ = AfterSuite(func() {
+	TestServer.Stop()
 })
 
 var _ = Describe("Main", func() {
@@ -46,15 +46,15 @@ var _ = Describe("Main", func() {
 	})
 
 	AfterEach(func() {
-		server.Clear()
+		TestServer.Clear()
 	})
 
 	It("Generate statistics of data from the execution", func() {
 		list := []string{
-			fmt.Sprintf(`%s -X POST -H "Content-type:application/json" -d '{"name":"talula"}'`,sutUrl("/A")),
-			fmt.Sprintf(`%s -X PUT -H "Content-type:application/json" -d '{"name":"talula"}'`,sutUrl("/A")),
-			fmt.Sprintf(`%s -X DELETE -H "Content-type:application/json" -d '{"name":"talula"}'`,sutUrl("/A")),
-			fmt.Sprintf(`%s -X GET`,sutUrl("/A")),
+			fmt.Sprintf(`%s -X POST -H "Content-type:application/json" -d '{"name":"talula"}'`, sutUrl("/A")),
+			fmt.Sprintf(`%s -X PUT -H "Content-type:application/json" -d '{"name":"talula"}'`, sutUrl("/A")),
+			fmt.Sprintf(`%s -X DELETE -H "Content-type:application/json" -d '{"name":"talula"}'`, sutUrl("/A")),
+			fmt.Sprintf(`%s -X GET`, sutUrl("/A")),
 		}
 
 		file := CreateFileFromLines(list)
@@ -77,7 +77,7 @@ var _ = Describe("Main", func() {
 		for _, method := range HTTP_METHODS_WITH_REQUEST_BODY {
 			It(fmt.Sprintf("in the body for verb %s", method), func() {
 				data := "a=1&b=2&c=3"
-				list := []string{fmt.Sprintf(`%s -X %s -d %s`,sutUrl("/A"), method, data)}
+				list := []string{fmt.Sprintf(`%s -X %s -d %s`, sutUrl("/A"), method, data)}
 				file := CreateFileFromLines(list)
 				defer os.Remove(file.Name())
 				cmd := exec.Command(exePath, "-f", file.Name())
@@ -89,14 +89,14 @@ var _ = Describe("Main", func() {
 				predicates = append(predicates, RequestWithPath("/A"))
 				predicates = append(predicates, RequestWithMethod(method))
 				predicates = append(predicates, RequestWithBody(data))
-				Expect(server.Find(predicates...)).To(Equal(true))
+				Expect(TestServer.Find(predicates...)).To(Equal(true))
 			})
 		}
 
 		It("in the querystring for verb GET", func() {
 			method := "GET"
 			data := "a=1&b=2&c=3"
-			list := []string{fmt.Sprintf(`%s -X %s -d %s"`,sutUrl("/A"), method, data)}
+			list := []string{fmt.Sprintf(`%s -X %s -d %s"`, sutUrl("/A"), method, data)}
 			file := CreateFileFromLines(list)
 			defer os.Remove(file.Name())
 			cmd := exec.Command(exePath, "-f", file.Name())
@@ -108,7 +108,7 @@ var _ = Describe("Main", func() {
 			predicates = append(predicates, RequestWithPath("/A"))
 			predicates = append(predicates, RequestWithMethod(method))
 			predicates = append(predicates, RequestWithQuerystring(data))
-			Expect(server.Find(predicates...)).To(Equal(true))
+			Expect(TestServer.Find(predicates...)).To(Equal(true))
 		})
 	})
 
@@ -116,7 +116,7 @@ var _ = Describe("Main", func() {
 		It(fmt.Sprintf("Makes a http %s request with http headers", method), func() {
 			applicationJson := "Content-Type:application/json"
 			applicationSoapXml := "Accept:application/soap+xml"
-			list := []string{fmt.Sprintf(`%s -X %s -H "%s" -H "%s"`,sutUrl("/A"), method, applicationJson, applicationSoapXml)}
+			list := []string{fmt.Sprintf(`%s -X %s -H "%s" -H "%s"`, sutUrl("/A"), method, applicationJson, applicationSoapXml)}
 			file := CreateFileFromLines(list)
 			defer os.Remove(file.Name())
 			cmd := exec.Command(exePath, "-f", file.Name())
@@ -129,20 +129,20 @@ var _ = Describe("Main", func() {
 			predicates = append(predicates, RequestWithMethod(method))
 			predicates = append(predicates, RequestWithHeader("Content-Type", "application/json"))
 			predicates = append(predicates, RequestWithHeader("Accept", "application/soap+xml"))
-			Expect(server.Find(predicates...)).To(Equal(true))
+			Expect(TestServer.Find(predicates...)).To(Equal(true))
 		})
 	}
 
 	for _, method := range SUPPORTED_HTTP_METHODS {
 		It(fmt.Sprintf("Makes a http %s request", method), func() {
-			list := []string{fmt.Sprintf(`%s -X %s`,sutUrl("/A"), method)}
+			list := []string{fmt.Sprintf(`%s -X %s`, sutUrl("/A"), method)}
 			file := CreateFileFromLines(list)
 			defer os.Remove(file.Name())
 			cmd := exec.Command(exePath, "-f", file.Name())
 			output, err := cmd.CombinedOutput()
 			fmt.Println(string(output))
 			Expect(err).To(BeNil())
-			Expect(server.Find(RequestWithPath("/A"), RequestWithMethod(method))).To(Equal(true))
+			Expect(TestServer.Find(RequestWithPath("/A"), RequestWithMethod(method))).To(Equal(true))
 		})
 	}
 
@@ -160,8 +160,8 @@ var _ = Describe("Main", func() {
 		fmt.Println(string(output))
 
 		Expect(err).To(BeNil())
-		Expect(server.Find(RequestWithPath("/A"))).To(Equal(true))
-		Expect(server.Find(RequestWithPath("/B"))).To(Equal(true))
-		Expect(server.Find(RequestWithPath("/C"))).To(Equal(true))
+		Expect(TestServer.Find(RequestWithPath("/A"))).To(Equal(true))
+		Expect(TestServer.Find(RequestWithPath("/B"))).To(Equal(true))
+		Expect(TestServer.Find(RequestWithPath("/C"))).To(Equal(true))
 	})
 })

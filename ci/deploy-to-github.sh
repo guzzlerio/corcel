af554710cb8423bbe22b6316236f1e456fffff1d
REAANDREW <email@andrewrea.co.uk>
2015-09-27 21:24:44 +0000

    For release and develop ensure the tag and release is deleted before creating the new one to ensure the files and revision are correct

--- ci/deploy-to-github.sh
+++ ci/deploy-to-github.sh
@@ -7,11 +7,11 @@ fi
 if [ "$TRAVIS_BRANCH" == "release" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-	ghr --username reaandrew --token $GITHUB_TOKEN --replace --prerelease pre-release dist/  
+	ghr --username reaandrew --token $GITHUB_TOKEN --delete --replace --prerelease pre-release dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-	ghr --username reaandrew --token $GITHUB_TOKEN --replace --prerelease latest dist/  
+	ghr --username reaandrew --token $GITHUB_TOKEN --delete --replace --prerelease latest dist/  
 fi

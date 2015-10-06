0711bd576d3b8958d22c7cac08a306d2dc0ffe05
Andrew Rea <email@andrewrea.co.uk>
2015-10-06 18:19:01 +0000

    Added tests on the requests reader for different numbers of concurrent readers to ensure that it is syncronized and threadsafe.  Needed to make use of the Mutex structs

--- RequestReader_test.go
+++ RequestReader_test.go
@@ -1,76 +1,80 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
-    "os"
-    "bufio"
+	"os"
+	"sync"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-type RequestStream interface{
-    Read() *http.Request
+type RequestStream interface {
+	Read() *http.Request
 }
 
-type SequentialStream struct{
-    reader *RequestReader
+type SequentialStream struct {
+	reader *RequestReader
 }
 
-func (instance *SequentialStream) Read() <-chan *http.Request{
-    ch := make(chan *http.Request)
-    go func(){
-        for i := 0; i < len(instance.reader.Requests); i++ {
-            request := instance.reader.Read(i)
-            ch <- request
-        }
-        close(ch)
-    }()
-    return ch
+func (instance *SequentialStream) Read() <-chan *http.Request {
+	ch := make(chan *http.Request)
+	go func() {
+		for i := 0; i < len(instance.reader.Requests); i++ {
+			request := instance.reader.Read(i)
+			ch <- request
+		}
+		close(ch)
+	}()
+	return ch
 }
 
-func NewSequentialStream(reader *RequestReader) *SequentialStream{
-    return &SequentialStream{
-        reader : reader,
-    }
+func NewSequentialStream(reader *RequestReader) *SequentialStream {
+	return &SequentialStream{
+		reader: reader,
+	}
 }
 
-type RequestReader struct{
-    Requests []*http.Request
+type RequestReader struct {
+	Requests []*http.Request
 }
 
-func NewRequestReader(filePath string) *RequestReader{
-    file, err := os.Open(filePath)
-    defer file.Close()
-    check(err)
-    requests := []*http.Request{}
+func NewRequestReader(filePath string) *RequestReader {
+	file, err := os.Open(filePath)
+	defer file.Close()
+	check(err)
+	requests := []*http.Request{}
 	requestAdapter := NewRequestAdapter()
 	scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        request, err := requestAdapter.Create(line)
-        requests = append(requests, request)
-        check(err)
-    }
+	for scanner.Scan() {
+		line := scanner.Text()
+		request, err := requestAdapter.Create(line)
+		requests = append(requests, request)
+		check(err)
+	}
 
-    return &RequestReader{
-        Requests : requests,
-    }
+	return &RequestReader{
+		Requests: requests,
+	}
 }
 
 func (instance *RequestReader) Read(index int) *http.Request {
-    return instance.Requests[index]
+	return instance.Requests[index]
 }
 
-func (instance *RequestReader) NewSequentialStream() *SequentialStream{
-    return NewSequentialStream(instance)
+func (instance *RequestReader) NewSequentialStream() *SequentialStream {
+	return NewSequentialStream(instance)
 }
 
 var _ = Describe("RequestReader", func() {
 
-	It(" Single reader iterates over lines in a file", func() {
-		list := []string{
+	var list []string
+	var reader *RequestReader
+
+	BeforeEach(func() {
+		list = []string{
 			fmt.Sprintf(`%s -X POST `, UrlForTestServer("/error")),
 			fmt.Sprintf(`%s -X POST `, UrlForTestServer("/success")),
 			fmt.Sprintf(`%s -X POST `, UrlForTestServer("/error")),
@@ -78,15 +82,39 @@ var _ = Describe("RequestReader", func() {
 			fmt.Sprintf(`%s -X POST `, UrlForTestServer("/error")),
 			fmt.Sprintf(`%s -X POST `, UrlForTestServer("/success")),
 		}
+		file := CreateFileFromLines(list)
+		file.Close()
+		reader = NewRequestReader(file.Name())
+	})
 
-	    file := CreateFileFromLines(list)
-        file.Close()
-        reader := NewRequestReader(file.Name())
-        requests := []*http.Request{}
-        stream := reader.NewSequentialStream()
-        for request := range(stream.Read()){
-            requests = append(requests, request)
-        }
-        Expect(len(requests)).To(Equal(len(list)))
-    })
+	It("Single reader iterates over lines in a file", func() {
+		requests := []*http.Request{}
+		stream := reader.NewSequentialStream()
+		for request := range stream.Read() {
+			requests = append(requests, request)
+		}
+		Expect(len(requests)).To(Equal(len(list)))
+	})
+
+    for _, numberOfWorkers := range NUMBER_OF_WORKERS_TO_TEST {
+		It(fmt.Sprintf("Multiple readers totalling %v iterates over lines in a file", numberOfWorkers), func() {
+			var wg sync.WaitGroup
+            var mutex = &sync.Mutex{}
+			wg.Add(numberOfWorkers)
+			requests := []*http.Request{}
+			for i := 0; i < numberOfWorkers; i++ {
+				go func() {
+					stream := reader.NewSequentialStream()
+					for request := range stream.Read() {
+                        mutex.Lock()
+						requests = append(requests, request)
+                        mutex.Unlock()
+					}
+                    wg.Done()
+				}()
+			}
+            wg.Wait()
+			Expect(len(requests)).To(Equal(len(list) * numberOfWorkers))
+		})
+	}
 })
--- main_test.go
+++ main_test.go
@@ -23,6 +23,7 @@ var (
 	RESPONSE_CODES_400             = []int{400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418}
 	RESPONSE_CODES_500             = []int{500, 501, 502, 503, 504, 505}
 	WAIT_TIME_TESTS                = []string{"1ms", "2ms", "4ms", "8ms", "16ms", "32ms", "64ms", "128ms"}
+    NUMBER_OF_WORKERS_TO_TEST      = []int{1,2,4,8,16,32,64,128,256}
 )
 
 func UrlForTestServer(path string) string {

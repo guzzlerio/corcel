2bde06556ff026f77a2c3c2b5574398631c93e57
Andrew Rea <email@andrewrea.co.uk>
2015-10-08 19:00:05 +0000

    Updating the build script to specify the branch in the ghr tool so that the releases are correctly updated

--- ci/deploy-to-github.sh
+++ ci/deploy-to-github.sh
@@ -1,17 +1,17 @@
 if [ "$TRAVIS_BRANCH" == "master" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-	ghr --username reaandrew --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
+	ghr --username reaandrew -c master --token $GITHUB_TOKEN --replace `git describe --abbrev=0 --tags` dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "release" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-	ghr --username reaandrew --token $GITHUB_TOKEN --delete --replace --prerelease pre-release dist/  
+	ghr --username reaandrew -c release --token $GITHUB_TOKEN --delete --replace --prerelease pre-release dist/  
 fi
 
 if [ "$TRAVIS_BRANCH" == "develop" ]
 then
     gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}"
-	ghr --username reaandrew --token $GITHUB_TOKEN --delete --replace --prerelease latest dist/  
+	ghr --username reaandrew -c develop --token $GITHUB_TOKEN --delete --replace --prerelease latest dist/  
 fi

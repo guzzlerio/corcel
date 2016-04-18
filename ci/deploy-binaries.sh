836eae19b9867cd654c1cb4920d6c38cc1512149
Andrew Rea <email@andrewrea.co.uk>
2016-04-18 07:46:06 +0000

    THe script was not sending the correct file

--- ci/deploy-binaries.sh
+++ ci/deploy-binaries.sh
@@ -5,5 +5,5 @@ do
     arch=${inArr[1]}
     version="$(git describe --abbrev=0)"
     token=$CORCEL_DEPLOY_TOKEN
-    curl -X POST -H "Content-Type: application/octet-stream" --data-binary "@data.output" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token"
+    curl -X POST -H "Content-Type: application/octet-stream" --data-binary "@dist/${i}" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token"
 done

9e1c0e906d559568925b3d26e32f9572aa58c588
Andrew Rea <email@andrewrea.co.uk>
2016-04-18 07:16:39 +0000

    Updated the deploy script and also updated the CI file to execute it

--- ci/deploy-binaries.sh
+++ ci/deploy-binaries.sh
@@ -5,5 +5,5 @@ do
     arch=${inArr[1]}
     version="$(git describe --abbrev=0)"
     token=$CORCEL_DEPLOY_TOKEN
-    echo "curl -X POST -H 'Content-Type: application/octet-stream' --data-binary '@data.output' 'https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token'"
+    curl -X POST -H "Content-Type: application/octet-stream" --data-binary "@data.output" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token"
 done

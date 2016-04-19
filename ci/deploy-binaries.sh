2dba2e2be8600f9637fe1af10b5eaab8f3ad3653
Andrew Rea <email@andrewrea.co.uk>
2016-04-19 19:48:27 +0000

    uploading the updated gitlab files and deploy

--- ci/deploy-binaries.sh
+++ ci/deploy-binaries.sh
@@ -3,8 +3,7 @@ do
     inArr=(${i//_/ })
     os=${inArr[0]}
     arch=${inArr[1]}
-    version="$(git describe --abbrev=0)"
+    version="$(git describe)"
     token=$CORCEL_DEPLOY_TOKEN
-    #tar -cvzf "${i}.tar.gz" "dist/${i}"
-    curl --retry 5 -X POST -H "Content-Type: application/octet-stream" --data-binary "@dist/${i}" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token"
+    curl --retry 5 -X POST -H "Content-Type: application/octet-stream" --data-binary "@$1/${i}" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token"
 done

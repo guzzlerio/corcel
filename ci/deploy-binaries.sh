ed50e735e8220fdefe192ae89041a1470136df15
Andrew Rea <email@andrewrea.co.uk>
2016-04-19 19:50:25 +0000

    configuring the latest flag

--- ci/deploy-binaries.sh
+++ ci/deploy-binaries.sh
@@ -5,5 +5,5 @@ do
     arch=${inArr[1]}
     version="$(git describe)"
     token=$CORCEL_DEPLOY_TOKEN
-    curl --retry 5 -X POST -H "Content-Type: application/octet-stream" --data-binary "@$1/${i}" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token"
+    curl --retry 5 -X POST -H "Content-Type: application/octet-stream" --data-binary "@$1/${i}" "https://corcel.io/releases?version=$version&arch=$arch&os=$os&token=$token&latest=$2"
 done

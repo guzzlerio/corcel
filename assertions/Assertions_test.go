659e71f109bbc94899c099fda7588f1ee3a4fc64
Andrew Rea <email@andrewrea.co.uk>
2016-06-11 13:44:20 +0000

    Exact Assertions Fails

--- assertions/Assertions_test.go
+++ assertions/Assertions_test.go
@@ -9,20 +9,39 @@ import (
 var _ = FDescribe("Assertions", func() {
 
 	It("Exact Assertion Succeeds", func() {
-		expectedKey := "some:key"
+		key := "some:key"
 		expectedValue := 7
 
 		executionResult := core.ExecutionResult{
-			expectedKey: expectedValue,
+			key: expectedValue,
 		}
 
 		assertion := ExactAssertion{
-			Key:      expectedKey,
+			Key:      key,
 			Expected: expectedValue,
 		}
 
 		result := assertion.Assert(executionResult)
 		Expect(result["result"]).To(Equal(true))
+		Expect(result["message"]).To(BeNil())
+	})
+
+	It("Exact Assertion Fails", func() {
+		key := "some:key"
+		expectedValue := 7
+
+		executionResult := core.ExecutionResult{
+			key: 8,
+		}
+
+		assertion := ExactAssertion{
+			Key:      key,
+			Expected: expectedValue,
+		}
+
+		result := assertion.Assert(executionResult)
+		Expect(result["result"]).To(Equal(false))
+		Expect(result["message"]).To(Equal("FAIL: 8 does not match 7"))
 	})
 
 })

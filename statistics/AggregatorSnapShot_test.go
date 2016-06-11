65c3f0c191652c7a1abaf8889fec323d1103197b
Andrew Rea <email@andrewrea.co.uk>
2016-06-11 09:45:23 +0000

    Updates an empty snap shot with gauges

--- statistics/AggregatorSnapShot_test.go
+++ statistics/AggregatorSnapShot_test.go
@@ -9,14 +9,22 @@ import (
 
 var _ = FDescribe("AggregatorSnapShot", func() {
 
+	var (
+		targetSnapShot  *AggregatorSnapShot
+		subjectSnapShot *AggregatorSnapShot
+	)
+
+	BeforeEach(func() {
+		targetSnapShot = NewAggregatorSnapShot()
+		subjectSnapShot = NewAggregatorSnapShot()
+	})
+
 	It("Updates an empty snap shot counters", func() {
 
 		key := "counter:1"
 		value := int64(101)
-		timeStamp := time.Now().UnixNano()
-		targetSnapShot := NewAggregatorSnapShot()
 
-		subjectSnapShot := NewAggregatorSnapShot()
+		timeStamp := time.Now().UnixNano()
 		subjectSnapShot.updateCounter(key, value)
 		subjectSnapShot.updateTime(timeStamp)
 
@@ -24,7 +32,24 @@ var _ = FDescribe("AggregatorSnapShot", func() {
 
 		Expect(len(targetSnapShot.Times)).To(Equal(1))
 		Expect(targetSnapShot.Times[0]).To(Equal(subjectSnapShot.Times[0]))
+		Expect(targetSnapShot.Counters[key]).ToNot(BeNil())
+		Expect(targetSnapShot.Counters[key]).To(Equal(subjectSnapShot.Counters[key]))
+	})
 
+	It("Updates an empty snap shot gauges", func() {
+		key := "gauge:1"
+		value := float64(102.1)
+
+		timeStamp := time.Now().UnixNano()
+		subjectSnapShot.updateGauge(key, value)
+		subjectSnapShot.updateTime(timeStamp)
+
+		targetSnapShot.Update(*subjectSnapShot)
+
+		Expect(len(targetSnapShot.Times)).To(Equal(1))
+		Expect(targetSnapShot.Times[0]).To(Equal(subjectSnapShot.Times[0]))
+		Expect(targetSnapShot.Gauges[key]).ToNot(BeNil())
+		Expect(targetSnapShot.Gauges[key]).To(Equal(subjectSnapShot.Gauges[key]))
 	})
 
 })

79b558a6453c8ffbe1323640464397d915ca8691
Andrew Rea <email@andrewrea.co.uk>
2016-06-12 11:42:23 +0000

    Added all pending tests required to test the greater than assertion.  The complexity comes in that you cannot use mathematical operators on types of interface{} so manual inspection is required hence why these rules are so long

--- assertions/Assertions_test.go
+++ assertions/Assertions_test.go
@@ -6,11 +6,194 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Assertions", func() {
+var _ = FDescribe("Assertions", func() {
+
+	key := "some:key"
+
+	Context("Greater Than Assertion", func() {
+
+		/*
+					   Test Required
+					                            INSTANCE
+
+			                                 nil      float64    int    string-number   string
+					             ACTUAL
+
+					             float64         x          x          x          x           √
+
+					             int             x          x          x          x           √
+
+					             string-number   x          x          x          x           √
+
+					             string          x          x          x          x           x
+
+					             nil             x          x          x          x           x
+
+		*/
+
+		//To set further context I am making the following assumption
+		//Something is greater than nil
+		//nil is NOT greater than nil
+		//nil is NOT greater than Something
+		//string which is not a number is NOT greater than any number
+		//number is NOT greater than a string which is not a number
+		//Attempts will first be made to parse strings into a float64
+		Context("Succeeds", func() {
+
+			It("When Actual is float64 and Instance is nil", func() {
+				key := "some:key"
+				actualValue := 1.1
+				var instanceValue struct{}
+
+				executionResult := core.ExecutionResult{
+					key: actualValue,
+				}
+
+				assertion := GreaterThanAssertion{
+					Key:   key,
+					Value: instanceValue,
+				}
+
+				result := assertion.Assert(executionResult)
+				Expect(result["result"]).To(Equal(true))
+				Expect(result["message"]).To(BeNil())
+			})
+
+			PIt("When Actual is int and Instance is nil", func() {
+
+			})
+
+			PIt("When Actual is string-number and Instance is nil", func() {
+
+			})
+
+			PIt("When Actual is string and Instance is nil", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is int and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is string-number and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is int and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is string-number and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is string and Instance is string", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is string-number", func() {
+
+			})
+
+			PIt("When Actual is int and Instance is string-number", func() {
+
+			})
+
+			PIt("When Actual is string-number and Instance is string-number", func() {
+
+			})
+		})
+
+		Context("Fails", func() {
+			PIt("When Actual is nil and Instance is nil", func() {
+
+			})
+
+			PIt("When Actual is nil and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is nil and Instance is string-number", func() {
+
+			})
+
+			PIt("When Actual is nil and Instance is string", func() {
+
+			})
+
+			PIt("When Actual is nil and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is int and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is string-number and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is int and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is string-number and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is string and Instance is int", func() {
+
+			})
+
+			PIt("When Actual is string and Instance is float64", func() {
+
+			})
+
+			PIt("When Actual is string and Instance is string-number", func() {
+
+			})
+
+			PIt("When Actual is string and Instance is string", func() {
+
+			})
+
+			PIt("When Actual is float64 and Instance is string", func() {
+
+			})
+
+			PIt("When Actual is int and Instance is string", func() {
+
+			})
+
+			PIt("When Actual string-number int and Instance is string", func() {
+
+			})
+
+		})
+
+	})
 
 	Context("Not Equal Assertion", func() {
-
-		key := "some:key"
 
 		It("Succeeds", func() {
 			executionResult := core.ExecutionResult{
@@ -26,6 +209,7 @@ var _ = Describe("Assertions", func() {
 			Expect(result["result"]).To(Equal(true))
 			Expect(result["message"]).To(BeNil())
 		})
+
 		It("Fails", func() {
 			executionResult := core.ExecutionResult{
 				key: 7,
@@ -44,7 +228,6 @@ var _ = Describe("Assertions", func() {
 
 	Context("Exact Assertion", func() {
 		It("Exact Assertion Succeeds", func() {
-			key := "some:key"
 			expectedValue := 7
 
 			executionResult := core.ExecutionResult{
@@ -62,7 +245,6 @@ var _ = Describe("Assertions", func() {
 		})
 
 		It("Exact Assertion Fails", func() {
-			key := "some:key"
 			expectedValue := 7
 
 			executionResult := core.ExecutionResult{
---
+++ assertions/GreaterThanAssertion.go
@@ -0,0 +1,97 @@
+package assertions
+
+import (
+	"fmt"
+	"strconv"
+
+	"ci.guzzler.io/guzzler/corcel/core"
+)
+
+//GreaterThanAssertion ...
+type GreaterThanAssertion struct {
+	Key   string
+	Value interface{}
+}
+
+func (instance *GreaterThanAssertion) resultKey() string {
+	return fmt.Sprintf("assert:greaterthan:%v:%v", instance.Key, instance.Value)
+}
+
+//Assert ...
+func (instance *GreaterThanAssertion) Assert(executionResult core.ExecutionResult) core.AssertionResult {
+	actual := executionResult[instance.Key]
+
+	result := map[string]interface{}{
+		"expected": instance.Value,
+		"actual":   actual,
+		"key":      instance.resultKey(),
+	}
+
+	// INEFFICIENT BUT WORKING ...
+	//var actualType interface{}
+	//var instanceType interface{}
+	switch actualType := actual.(type) {
+	case float64:
+		switch instanceType := instance.Value.(type) {
+		case float64:
+			result["result"] = actualType > instanceType
+			break
+		case int:
+			result["result"] = actualType > float64(instanceType)
+			break
+		case string:
+			value, err := strconv.ParseFloat(instanceType, 64)
+			if err != nil {
+				result["result"] = false
+			} else {
+				result["result"] = actualType > value
+			}
+		default:
+			result["result"] = true
+		}
+	case int:
+		switch instanceType := instance.Value.(type) {
+		case float64:
+			result["result"] = float64(actualType) > instanceType
+		case int:
+			result["result"] = actualType > instanceType
+		case string:
+			value, err := strconv.ParseFloat(instanceType, 64)
+			if err != nil {
+				result["result"] = false
+			} else {
+				result["result"] = float64(actualType) > value
+			}
+		}
+	case string:
+		switch instanceType := instance.Value.(type) {
+		case float64:
+			value, err := strconv.ParseFloat(actualType, 64)
+			if err != nil {
+				result["result"] = false
+			} else {
+				result["result"] = value > instanceType
+			}
+		case int:
+			value, err := strconv.ParseFloat(actualType, 64)
+			if err != nil {
+				result["result"] = false
+			} else {
+				result["result"] = value > float64(instanceType)
+			}
+		case string:
+			actualStrValue, actualStrErr := strconv.ParseFloat(actualType, 64)
+			instanceStrValue, instanceStrErr := strconv.ParseFloat(instanceType, 64)
+			if actualStrErr != nil && instanceStrErr != nil {
+				result["result"] = actualType > instanceType
+			} else {
+				result["result"] = actualStrValue > instanceStrValue
+			}
+		}
+	}
+
+	if result["result"].(bool) == false {
+		result["message"] = fmt.Sprintf("FAIL: %v is not greater %v", actual, instance.Value)
+	}
+	return result
+}

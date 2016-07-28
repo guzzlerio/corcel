159270b1d39a001ca98f23e0462c2649a38c9b7d
Andrew Rea <email@andrewrea.co.uk>
2016-07-28 10:07:18 +0000

    Tested uneven lists

--- processor/ListRingIterator_test.go
+++ processor/ListRingIterator_test.go
@@ -7,29 +7,41 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+func getPeopleCollection() []map[string]interface{} {
+	return []map[string]interface{}{
+		map[string]interface{}{
+			"name": "bob",
+			"age":  30,
+		},
+		map[string]interface{}{
+			"name": "carol",
+			"age":  31,
+		},
+		map[string]interface{}{
+			"name": "alice",
+			"age":  32,
+		},
+	}
+}
+
+func getProductsCollection() []map[string]interface{} {
+	return []map[string]interface{}{
+		map[string]interface{}{
+			"name": "toaster",
+			"sku":  "1234",
+		},
+		map[string]interface{}{
+			"name": "grinder",
+			"sku":  "5678",
+		},
+	}
+}
+
 var _ = Describe("ListRingIterator", func() {
 
 	It("Loops round", func() {
-		peopleKey := "People"
 		data := map[string][]map[string]interface{}{}
-		data[peopleKey] = []map[string]interface{}{}
-
-		bob := map[string]interface{}{
-			"name": "bob",
-			"age":  30,
-		}
-		carol := map[string]interface{}{
-			"name": "carol",
-			"age":  31,
-		}
-		alice := map[string]interface{}{
-			"name": "alice",
-			"age":  32,
-		}
-
-		data[peopleKey] = append(data[peopleKey], bob)
-		data[peopleKey] = append(data[peopleKey], carol)
-		data[peopleKey] = append(data[peopleKey], alice)
+		data["People"] = getPeopleCollection()
 
 		iterator := NewListRingIterator(data)
 
@@ -44,6 +56,32 @@ var _ = Describe("ListRingIterator", func() {
 		values3 := iterator.Values()
 		Expect(values3["$People.name"]).To(Equal("alice"))
 		Expect(values3["$People.age"]).To(Equal(32))
+	})
+
+	It("Loops around uneven lists", func() {
+		data := map[string][]map[string]interface{}{}
+		data["People"] = getPeopleCollection()
+		data["Products"] = getProductsCollection()
+
+		iterator := NewListRingIterator(data)
+
+		values1 := iterator.Values()
+		Expect(values1["$People.name"]).To(Equal("bob"))
+		Expect(values1["$People.age"]).To(Equal(30))
+		Expect(values1["$Products.name"]).To(Equal("toaster"))
+		Expect(values1["$Products.sku"]).To(Equal("1234"))
+
+		values2 := iterator.Values()
+		Expect(values2["$People.name"]).To(Equal("carol"))
+		Expect(values2["$People.age"]).To(Equal(31))
+		Expect(values2["$Products.name"]).To(Equal("grinder"))
+		Expect(values2["$Products.sku"]).To(Equal("5678"))
+
+		values3 := iterator.Values()
+		Expect(values3["$People.name"]).To(Equal("alice"))
+		Expect(values3["$People.age"]).To(Equal(32))
+		Expect(values3["$Products.name"]).To(Equal("toaster"))
+		Expect(values3["$Products.sku"]).To(Equal("1234"))
 	})
 
 })

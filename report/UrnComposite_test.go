888670a9e89732f556b620a98e84a323c27d3fa9
Andrew Rea <email@andrewrea.co.uk>
2016-09-10 12:12:59 +0000

    Another test to check for multiple root elements

--- report/UrnComposite_test.go
+++ report/UrnComposite_test.go
@@ -1,7 +1,6 @@
 package report
 
 import (
-	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -25,7 +24,7 @@ func createNode(name string) *node {
 	}
 }
 
-func createUrnAggregate(urns ...string) *node {
+func createUrnAggregate(urns ...string) (*node, error) {
 
 	var root *node
 	var next *node
@@ -35,11 +34,15 @@ func createUrnAggregate(urns ...string) *node {
 		if root != nil {
 			next = root
 		}
-		for _, item := range split {
+		for index, item := range split {
 			if root == nil {
 				root = createNode(item)
 				next = root
 				continue
+			}
+
+			if root != nil && index == 0 && item != root.Name {
+				return nil, fmt.Errorf("Multiple root elements")
 			}
 
 			if item == next.Name {
@@ -64,25 +67,32 @@ func createUrnAggregate(urns ...string) *node {
 		}
 	}
 
-	return root
+	return root, nil
 }
 
 var _ = Describe("UrnComposite", func() {
 
-	It("does something", func() {
+	It("Combines two urns into a composite", func() {
 		urn1 := "urn:a:b:c"
 		urn2 := "urn:a:b:d"
 
-		aggregate := createUrnAggregate(urn1, urn2)
+		aggregate, err := createUrnAggregate(urn1, urn2)
 
-		data, _ := json.MarshalIndent(aggregate, "", "  ")
-
-		fmt.Println(string(data))
-
+		Expect(err).To(BeNil())
 		Expect(aggregate.Name).To(Equal("urn"))
 		Expect(len(aggregate.Children)).To(Equal(1))
 		Expect(len(aggregate.Child(0).Child(0).Children)).To(Equal(2))
+	})
 
+	It("Detects multiple root elements", func() {
+
+		urn1 := "urn:a:b:c"
+		urn2 := "uri:a:b:d"
+
+		_, err := createUrnAggregate(urn1, urn2)
+
+		Expect(err).ToNot(BeNil())
+		Expect(err).To(MatchError("Multiple root elements"))
 	})
 
 })

5cc114243fd551f02b2a118487ca4372143033a7
Andrew Rea <email@andrewrea.co.uk>
2017-01-14 23:10:28 +0000

    Added Path utilities for finding a file walking up the parent directories starting from the working directory

---
+++ utils/PathUtils_test.go
@@ -0,0 +1,86 @@
+package utils
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Acceptance", func() {
+
+	var dir string
+
+	var createTestFile = func(tmpPath string) {
+		os.MkdirAll(tmpPath, os.ModePerm)
+		content := []byte("temporary file's content")
+		testFilePath := filepath.Join(tmpPath, "temp.txt")
+		if err := ioutil.WriteFile(testFilePath, content, 0666); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	BeforeEach(func() {
+		var err error
+		dir, err = ioutil.TempDir("", "corcel")
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	AfterEach(func() {
+		os.RemoveAll(dir) // clean up
+	})
+
+	It("Finds file at same depth", func() {
+		targetPath := filepath.Join(dir, "1", "2")
+
+		startingPath := targetPath
+
+		os.MkdirAll(startingPath, os.ModePerm)
+
+		os.Chdir(startingPath)
+
+		createTestFile(targetPath)
+
+		var result = FindFileUp("temp.txt")
+
+		Expect(result).ToNot(Equal(""))
+	})
+
+	It("Finds file at first parent", func() {
+		targetPath := filepath.Join(dir, "1")
+
+		startingPath := filepath.Join(dir, "1", "2")
+
+		os.MkdirAll(startingPath, os.ModePerm)
+
+		os.Chdir(startingPath)
+
+		createTestFile(targetPath)
+
+		var result = FindFileUp("temp.txt")
+
+		Expect(result).ToNot(Equal(""))
+	})
+
+	It("Finds file at second parent", func() {
+		targetPath := filepath.Join(dir)
+
+		startingPath := filepath.Join(dir, "1", "2")
+
+		os.MkdirAll(startingPath, os.ModePerm)
+
+		os.Chdir(startingPath)
+
+		createTestFile(targetPath)
+
+		var result = FindFileUp("temp.txt")
+
+		Expect(result).ToNot(Equal(""))
+	})
+
+})

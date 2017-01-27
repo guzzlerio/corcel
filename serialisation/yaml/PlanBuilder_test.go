5523e14256ae0c046f1a52873f3c77a647f066b2
jamlen <code@james-allen.co.uk>
2017-01-27 09:41:14 +0000

    Tests for PlanBuilder before/after

--- serialisation/yaml/PlanBuilder_test.go
+++ serialisation/yaml/PlanBuilder_test.go
@@ -24,22 +24,39 @@ duration: 0s
 
 		Expect(string(planData)).To(MatchYAML(expected))
 	})
-	It("builds a plan with waitTime", func() {
-		planBuilder := NewPlanBuilder()
+	Describe("waitTime", func() {
 
-		planBuilder.SetWaitTime("10s")
+		// Skipping this because gomega is unable to assert the panic if
+		// the func returns a value.
+		// Suggest switching to another assertion lib, but something like
+		// github.com/stretchr/testify requires the *testing.T which
+		// ginkgo does not take in
+		PIt("panics when waitTime does not parse", func() {
+			planBuilder := NewPlanBuilder()
 
-		plan := planBuilder.Build()
-		planData, _ := yaml.Marshal(&plan)
+			planBuilder.SetWaitTime("talula")
 
-		var expected = `
-random: false
-waitTime: 10s
-workers: 1
-duration: 0s
-`
+			// assert.Panics(t, func() {
+			// 	planBuilder.Build()
+			// }, "Should panic")
+		})
+		It("builds a plan with waitTime", func() {
+			planBuilder := NewPlanBuilder()
 
-		Expect(string(planData)).To(MatchYAML(expected))
+			planBuilder.SetWaitTime("10s")
+
+			plan := planBuilder.Build()
+			planData, _ := yaml.Marshal(&plan)
+
+			var expected = `
+  random: false
+  waitTime: 10s
+  workers: 1
+  duration: 0s
+  `
+
+			Expect(string(planData)).To(MatchYAML(expected))
+		})
 	})
 	It("builds a plan with a name", func() {
 		planBuilder := NewPlanBuilder()
@@ -59,15 +76,90 @@ duration: 0s
 
 		Expect(string(planData)).To(MatchYAML(expected))
 	})
-	It("builds a plan with a job", func() {
+	It("builds a plan with a before", func() {
 		planBuilder := NewPlanBuilder()
 
-		planBuilder.CreateJob().WithName("Some Job")
+		planBuilder.WithName("Some Plan").
+			AddBefore(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
 
 		plan := planBuilder.Build()
 		planData, _ := yaml.Marshal(&plan)
 
 		var expected = `
+name: Some Plan
+random: false
+waitTime: 0s
+workers: 1
+duration: 0s
+before:
+ - results:
+     value:1: talula 123 bang bang
+   type: DummyAction
+`
+
+		Expect(string(planData)).To(MatchYAML(expected))
+	})
+	It("builds a plan with a after", func() {
+		planBuilder := NewPlanBuilder()
+
+		planBuilder.WithName("Some Plan").
+			AddAfter(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
+
+		plan := planBuilder.Build()
+		planData, _ := yaml.Marshal(&plan)
+
+		var expected = `
+name: Some Plan
+random: false
+waitTime: 0s
+workers: 1
+duration: 0s
+after:
+ - results:
+     value:1: talula 123 bang bang
+   type: DummyAction
+`
+
+		Expect(string(planData)).To(MatchYAML(expected))
+	})
+	It("builds a plan with a before/after", func() {
+		planBuilder := NewPlanBuilder()
+
+		planBuilder.WithName("Some Plan").
+			AddBefore(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
+			AddAfter(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
+
+		plan := planBuilder.Build()
+		planData, _ := yaml.Marshal(&plan)
+
+		var expected = `
+name: Some Plan
+random: false
+waitTime: 0s
+workers: 1
+duration: 0s
+before:
+ - results:
+     value:1: talula 123 bang bang
+   type: DummyAction
+after:
+ - results:
+     value:1: talula 123 bang bang
+   type: DummyAction
+`
+
+		Expect(string(planData)).To(MatchYAML(expected))
+	})
+	Describe("jobs", func() {
+		It("builds a plan with a job", func() {
+			planBuilder := NewPlanBuilder()
+
+			planBuilder.CreateJob().WithName("Some Job")
+
+			plan := planBuilder.Build()
+			planData, _ := yaml.Marshal(&plan)
+
+			var expected = `
 random: false
 waitTime: 0s
 workers: 1
@@ -76,19 +168,49 @@ jobs:
 - name: Some Job
 `
 
-		Expect(string(planData)).To(MatchYAML(expected))
-	})
-	It("builds a plan with a step", func() {
-		planBuilder := NewPlanBuilder()
+			Expect(string(planData)).To(MatchYAML(expected))
+		})
+		It("builds a plan with a before/after job", func() {
+			planBuilder := NewPlanBuilder()
 
-		planBuilder.
-			CreateJob().WithName("Some Job").
-			CreateStep().WithName("Some Step")
+			planBuilder.CreateJob().WithName("Some Job").
+				AddBefore(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
+				AddAfter(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
 
-		plan := planBuilder.Build()
-		planData, _ := yaml.Marshal(&plan)
+			plan := planBuilder.Build()
+			planData, _ := yaml.Marshal(&plan)
 
-		var expected = `
+			var expected = `
+random: false
+waitTime: 0s
+workers: 1
+duration: 0s
+jobs:
+- name: Some Job
+  before:
+  - results:
+      value:1: talula 123 bang bang
+    type: DummyAction
+  after:
+  - results:
+      value:1: talula 123 bang bang
+    type: DummyAction
+`
+
+			Expect(string(planData)).To(MatchYAML(expected))
+		})
+		Describe("steps", func() {
+			It("builds a plan with a step", func() {
+				planBuilder := NewPlanBuilder()
+
+				planBuilder.
+					CreateJob().WithName("Some Job").
+					CreateStep().WithName("Some Step")
+
+				plan := planBuilder.Build()
+				planData, _ := yaml.Marshal(&plan)
+
+				var expected = `
 random: false
 waitTime: 0s
 workers: 1
@@ -99,20 +221,51 @@ jobs:
   - name: Some Step
 `
 
-		Expect(string(planData)).To(MatchYAML(expected))
-	})
-	It("builds a plan with an action", func() {
-		planBuilder := NewPlanBuilder()
+				Expect(string(planData)).To(MatchYAML(expected))
+			})
+			It("builds a plan with a before/after job", func() {
+				planBuilder := NewPlanBuilder()
 
-		planBuilder.
-			CreateJob().WithName("Some Job").
-			CreateStep().WithName("Some Step").
-			ToExecuteAction(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
+				planBuilder.CreateJob().WithName("Some Job").
+					CreateStep().WithName("Some Step").
+					AddBefore(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
+					AddAfter(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
 
-		plan := planBuilder.Build()
-		planData, _ := yaml.Marshal(&plan)
+				plan := planBuilder.Build()
+				planData, _ := yaml.Marshal(&plan)
 
-		var expected = `
+				var expected = `
+random: false
+waitTime: 0s
+workers: 1
+duration: 0s
+jobs:
+- name: Some Job
+  steps:
+  - name: Some Step
+    before:
+    - results:
+        value:1: talula 123 bang bang
+      type: DummyAction
+    after:
+    - results:
+        value:1: talula 123 bang bang
+      type: DummyAction
+`
+				Expect(string(planData)).To(MatchYAML(expected))
+			})
+			It("builds a plan with an action", func() {
+				planBuilder := NewPlanBuilder()
+
+				planBuilder.
+					CreateJob().WithName("Some Job").
+					CreateStep().WithName("Some Step").
+					ToExecuteAction(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build())
+
+				plan := planBuilder.Build()
+				planData, _ := yaml.Marshal(&plan)
+
+				var expected = `
 random: false
 waitTime: 0s
 workers: 1
@@ -127,21 +280,21 @@ jobs:
     name: Some Step
 `
 
-		Expect(string(planData)).To(MatchYAML(expected))
-	})
-	It("builds a plan with an assertion", func() {
-		planBuilder := NewPlanBuilder()
+				Expect(string(planData)).To(MatchYAML(expected))
+			})
+			It("builds a plan with an assertion", func() {
+				planBuilder := NewPlanBuilder()
 
-		planBuilder.
-			CreateJob().WithName("Some Job").
-			CreateStep().WithName("Some Step").
-			ToExecuteAction(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
-			WithAssertion(planBuilder.ExactAssertion("regex:match:1", "123"))
+				planBuilder.
+					CreateJob().WithName("Some Job").
+					CreateStep().WithName("Some Step").
+					ToExecuteAction(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
+					WithAssertion(planBuilder.ExactAssertion("regex:match:1", "123"))
 
-		plan := planBuilder.Build()
-		planData, _ := yaml.Marshal(&plan)
+				plan := planBuilder.Build()
+				planData, _ := yaml.Marshal(&plan)
 
-		var expected = `
+				var expected = `
 random: false
 waitTime: 0s
 workers: 1
@@ -160,22 +313,22 @@ jobs:
     name: Some Step
 `
 
-		Expect(string(planData)).To(MatchYAML(expected))
-	})
-	It("builds a plan with an extractor", func() {
-		planBuilder := NewPlanBuilder()
+				Expect(string(planData)).To(MatchYAML(expected))
+			})
+			It("builds a plan with an extractor", func() {
+				planBuilder := NewPlanBuilder()
 
-		planBuilder.WithName("Some Plan").
-			CreateJob().WithName("Some Job").
-			CreateStep().WithName("Some Step").
-			ToExecuteAction(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
-			WithExtractor(planBuilder.RegexExtractor().Name("regex:match:1").Key("value:1").Match("\\d+").Build()).
-			WithAssertion(planBuilder.ExactAssertion("regex:match:1", "123"))
+				planBuilder.WithName("Some Plan").
+					CreateJob().WithName("Some Job").
+					CreateStep().WithName("Some Step").
+					ToExecuteAction(planBuilder.DummyAction().Set("value:1", "talula 123 bang bang").Build()).
+					WithExtractor(planBuilder.RegexExtractor().Name("regex:match:1").Key("value:1").Match("\\d+").Build()).
+					WithAssertion(planBuilder.ExactAssertion("regex:match:1", "123"))
 
-		plan := planBuilder.Build()
-		planData, _ := yaml.Marshal(&plan)
+				plan := planBuilder.Build()
+				planData, _ := yaml.Marshal(&plan)
 
-		var expected = `
+				var expected = `
 name: Some Plan
 random: false
 waitTime: 0s
@@ -200,6 +353,8 @@ jobs:
     name: Some Step
 `
 
-		Expect(string(planData)).To(MatchYAML(expected))
+				Expect(string(planData)).To(MatchYAML(expected))
+			})
+		})
 	})
 })

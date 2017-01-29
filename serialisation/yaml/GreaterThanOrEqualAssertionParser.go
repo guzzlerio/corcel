0043204f8826402e92037d3163811f0e07fe6079
Andrew Rea <email@andrewrea.co.uk>
2017-01-29 19:21:01 +0000

    Adding failure case for greater than assertion parser

--- serialisation/yaml/GreaterThanOrEqualAssertionParser.go
+++ serialisation/yaml/GreaterThanOrEqualAssertionParser.go
@@ -1,6 +1,8 @@
 package yaml
 
 import (
+	"errors"
+
 	"github.com/guzzlerio/corcel/assertions"
 	"github.com/guzzlerio/corcel/core"
 )
@@ -9,7 +11,16 @@ import (
 type GreaterThanOrEqualAssertionParser struct{}
 
 //Parse ...
-func (instance GreaterThanOrEqualAssertionParser) Parse(input map[string]interface{}) core.Assertion {
+func (instance GreaterThanOrEqualAssertionParser) Parse(input map[string]interface{}) (core.Assertion, error) {
+
+	if _, ok := input["key"]; !ok {
+		return nil, errors.New("key is not present")
+	}
+
+	if _, ok := input["expected"]; !ok {
+		return nil, errors.New("expected is not present")
+	}
+
 	return &assertions.GreaterThanOrEqualAssertion{
 		Key:   input["key"].(string),
 		Value: input["expected"],
--- serialisation/yaml/GreaterThanOrEqualAssertionParser_test.go
+++ serialisation/yaml/GreaterThanOrEqualAssertionParser_test.go
@@ -1,12 +1,14 @@
 package yaml
 
 import (
+	"fmt"
+
 	"github.com/guzzlerio/corcel/assertions"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("GreaterThanOrEqualAssertionParser", func() {
+var _ = FDescribe("GreaterThanOrEqualAssertionParser", func() {
 	It("Parses", func() {
 
 		var expectedKey = "talula"
@@ -16,13 +18,39 @@ var _ = Describe("GreaterThanOrEqualAssertionParser", func() {
 		}
 
 		var parser = GreaterThanOrEqualAssertionParser{}
-		var assertion = parser.Parse(input).(*assertions.GreaterThanOrEqualAssertion)
+		assertion, err := parser.Parse(input)
+		var gteAssertion = assertion.(*assertions.GreaterThanOrEqualAssertion)
 
-		Expect(assertion.Key).To(Equal(expectedKey))
-		Expect(assertion.Value).To(Equal(7))
+		Expect(err).To(BeNil())
+		Expect(gteAssertion.Key).To(Equal(expectedKey))
+		Expect(gteAssertion.Value).To(Equal(7))
 	})
 
 	It("Returns Key", func() {
 		Expect(GreaterThanOrEqualAssertionParser{}.Key()).To(Equal("GreaterThanOrEqualAssertion"))
+	})
+
+	It("Fails to parse without key", func() {
+		var input = map[string]interface{}{
+			"boom":     "talula",
+			"expected": 7,
+		}
+
+		var parser = GreaterThanOrEqualAssertionParser{}
+		_, err := parser.Parse(input)
+		Expect(err).ToNot(BeNil())
+		Expect(fmt.Sprintf("%v", err)).To(ContainSubstring("key is not present"))
+	})
+
+	It("Fails to parse without expected", func() {
+		var input = map[string]interface{}{
+			"key":  "talula",
+			"boom": 7,
+		}
+
+		var parser = GreaterThanOrEqualAssertionParser{}
+		_, err := parser.Parse(input)
+		Expect(err).ToNot(BeNil())
+		Expect(fmt.Sprintf("%v", err)).To(ContainSubstring("expected is not present"))
 	})
 })

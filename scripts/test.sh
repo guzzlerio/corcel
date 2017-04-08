9b4a8c60f8af9e8022cd0f8d18cdd1f3d959ab10
Andrew Rea <email@andrewrea.co.uk>
2017-04-08 23:38:48 +0000

    Fixing the starting coverage file

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-rm coverage.txt 2> /dev/null
-touch coverage.txt
+printf "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
